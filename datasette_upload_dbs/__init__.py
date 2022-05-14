1ce595dbe36e7b8fe05c6cdc17d7a46e75909e65
Simon Willison <swillison@gmail.com>
2022-05-13 17:47:32 -0700

    Initial implementation, no tests yet - refs #1

--- datasette_upload_dbs/__init__.py
+++ datasette_upload_dbs/__init__.py
@@ -1,1 +1,103 @@
 from datasette import hookimpl
+from datasette.database import Database
+from datasette.utils.asgi import Response, Forbidden
+from datasette.utils.sqlite import sqlite3
+from starlette.requests import Request
+from shutil import copyfileobj
+import pathlib
+
+
+@hookimpl
+def permission_allowed(actor, action):
+    if action == "upload-dbs" and actor and actor.get("id") == "root":
+        return True
+
+
+@hookimpl
+def register_routes():
+    return [
+        (r"^/-/upload-dbs$", upload_dbs),
+        (r"^/-/upload-db$", lambda: Response.redirect("/-/upload-dbs")),
+    ]
+
+
+@hookimpl
+def menu_links(datasette, actor):
+    async def inner():
+        if await datasette.permission_allowed(actor, "upload-dbs", default=False):
+            return [
+                {
+                    "href": datasette.urls.path("/-/upload-dbs"),
+                    "label": "Upload database",
+                },
+            ]
+
+    return inner
+
+
+async def upload_dbs(scope, receive, datasette, request):
+    if not await datasette.permission_allowed(
+        request.actor, "upload-dbs", default=False
+    ):
+        raise Forbidden("Permission denied for upload-dbs")
+
+    if request.method != "POST":
+        return Response.html(
+            await datasette.render_template("upload_dbs.html", request=request)
+        )
+
+    # We use the Starlette request object to handle file uploads
+    starlette_request = Request(scope, receive)
+
+    async def error(msg):
+        if is_xhr:
+            return Response.json(
+                {"ok": False, "error": "File is not a valid SQLite database"}
+            )
+
+        return Response.html(
+            await datasette.render_template(
+                "upload_dbs.html",
+                {
+                    "error": "File is not a valid SQLite database",
+                },
+                request=request,
+            )
+        )
+
+    formdata = await starlette_request.form()
+    db_file = formdata["db"]
+    is_xhr = formdata.get("xhr")
+    db_name = (formdata.get("db_name") or "").strip()
+
+    if not db_name:
+        db_name = db_file.filename.split(".")[0]
+
+    first_16 = db_file.file.read(16)
+    if first_16 != b"SQLite format 3\x00":
+        return await error("File is not a valid SQLite database (invalid header)")
+
+    # Copy it to its final destination
+    filepath = pathlib.Path(".") / (db_name + ".db")
+    with open(filepath, "wb+") as target_file:
+        db_file.file.seek(0)
+        copyfileobj(db_file.file, target_file)
+
+    # Now really verify it
+    conn = sqlite3.connect(str(filepath))
+    try:
+        conn.execute("select * from sqlite_master")
+    except sqlite3.Error as e:
+        # Delete file, it is invalid
+        filepath.unlink()
+        return await error(f"File is not a valid SQLite database ({e})")
+
+    # File is valid - add it to this Datasette instance
+    db = Database(datasette, path=str(filepath))
+    datasette.add_database(db)
+
+    redirect_url = datasette.urls.database(db.name)
+    if is_xhr:
+        return Response.json({"ok": True, "redirect": redirect_url})
+    else:
+        return Response.redirect(redirect_url)
--- setup.py
+++ setup.py
@@ -27,15 +27,13 @@ setup(
     license="Apache License, Version 2.0",
     classifiers=[
         "Framework :: Datasette",
-        "License :: OSI Approved :: Apache Software License"
+        "License :: OSI Approved :: Apache Software License",
     ],
     version=VERSION,
     packages=["datasette_upload_dbs"],
     entry_points={"datasette": ["upload_dbs = datasette_upload_dbs"]},
-    install_requires=["datasette"],
+    install_requires=["datasette", "starlette"],
     extras_require={"test": ["pytest", "pytest-asyncio"]},
-    package_data={
-        "datasette_upload_dbs": ["templates/*"]
-    },
+    package_data={"datasette_upload_dbs": ["templates/*"]},
     python_requires=">=3.7",
 )
